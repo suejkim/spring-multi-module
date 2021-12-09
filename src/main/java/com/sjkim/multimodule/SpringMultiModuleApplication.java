017c0c05c7785605b195c56efc87ac4c30828f9e
sjkim <abbccc0225@gmail.com>
2021-12-09 21:44:21 +0900

    initial commit, .gitignore

---
+++ src/main/java/com/sjkim/multimodule/SpringMultiModuleApplication.java
@@ -0,0 +1,13 @@
+package com.sjkim.multimodule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMultiModuleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringMultiModuleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sjkim/multimodule/SpringMultiModuleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sjkim.multimodule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMultiModuleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
